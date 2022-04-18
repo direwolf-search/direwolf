81209b6e0d1319148843b6e860043c9a8cbd984b
Alexey Matveev <himei@tuta.io>
2022-04-19 01:38:09 +0300

    refactor: issue_12
    Method engine.VisitAll(context.Context, ...string) removed to engine.Visit(context.Context, ...string)
    Slice iteration excluded
    HTMLParser.ParseHTML() changed
    
    Closes issue_12

--- internal/domain/service/crawler/engine.go
+++ internal/domain/service/crawler/engine.go
@@ -11,8 +11,7 @@ type Engine interface {
 	SetParallelism(workersNum int)
 	SetTorGate(gate string)
 	SetHTMLParser(parser interface{})
-	Visit(ctx context.Context, url string)
-	VisitAll(ctx context.Context, urls ...string)
+	Visit(ctx context.Context, urls ...string)
 	SaveLink(ctx context.Context, l map[string]interface{}) error
 	SaveHost(ctx context.Context, h map[string]interface{}) error
 	SetQueue()
